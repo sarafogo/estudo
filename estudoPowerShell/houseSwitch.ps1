47c720a1b179f85e654d6215e8470e7cfe39de4f
Sara Fogo Silva Simão <sara.f.silva.simao@avanade.com>
2025-02-13 18:05:49 -0300

    arquivo de estudo de powershell

---
+++ estudoPowerShell/houseSwitch.ps1
@@ -0,0 +1,6 @@
+﻿$House = "Stark"
+Switch($House) {
+    "Targaryen" { Write-Host "You're crazy!"; break}
+    "Lannister" { Write-Host "You always pay your debts!"; break}
+    "Stark" {Write-Host "The winter is coming, right!?"; break}
+}
