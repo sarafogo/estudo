7cf9468b2624290ed19f18916183b206c0c427e0
Sara Fogo Silva Simão <sara.f.silva.simao@avanade.com>
2025-04-08 17:39:08 -0300

    arquivos de estudo sobre .net e C#

--- estudoDotNet/anotacoes.md
+++ estudoDotNet/anotacoes.md
@@ -2,3 +2,29 @@
 |  :-----| :----------|
 | versão: v.5 + | Versão: v. <= 4.8.1 |
 
+- Liguagem de tipagem estática = não é possivel converter tipos das variáveis. 
+
+### Tipos mais comuns
+- bool: true or false
+    - b, b2
+- char: Representa um caractere UTF-16 (um caractere de uma string)
+    - c
+- byte: Não representa valores negativos, valores de 0 a 255
+- int: representa um inteiro, valores entre -2bi e 2bi
+- long: representa um inteiro de 64-bit
+- float: ponto flutuante
+- double: ponto flutuante de dupla precisão
+- decimal: mais precisão ainda, representa melhor moeda
+- string: sequência de caracteres UTF-16
+
+### Operadores e expressões:
+
+incremento: i++;
+decremento i--;
+expressão: dentro do método ou de uma variável.
+
+Retornar um valor booleano:
+i == (binário) 42 ? 10 : 20; 
+
+### array
+[sharplab.io]
--- estudoDotNet/cursocsharp/Program.cs
+++ estudoDotNet/cursocsharp/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Console.Write("Digite seu nome: ");
+// string name = Console.ReadLine();
+// Console.WriteLine("Olá, " + name);
+// Console.Write("Digite o ano do seu nascimento: ");
+// int year = int.Parse(Console.ReadLine());
+// int age = 2025 - year;
+// Console.WriteLine($"Você tem  {age} anos.");
+
+// if (age >= 18)
+// {
+//     Console.WriteLine("Você é maior de idade");
+// }
+// else
+// {
+//     Console.WriteLine("Você é menor de idade");
+// }
+
+// if (age >= 18 && name == "Sara")
+// {
+//     Console.WriteLine("Você é maior de idade");
+// }
+// else
+// {
+//     Console.WriteLine("Você é menor de idade");
+// }
+
+class Program
+{
+    static void Main(string[] a)
+    {
+        string[] names = { "Sara", "Maria", "João" };
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine(names[i]);
+        }
+        
+    }
+}
+
